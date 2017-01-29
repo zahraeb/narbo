6c8f17915b19511cf56ebcf354e86ffc9e24b602
zahraeb <z.ebrahimi717@rocketmail.com>
2017-01-29 15:12:10 +0330

    update data

--- app/src/main/java/narbo/ir/narbo/Translator/MainActivity.java
+++ app/src/main/java/narbo/ir/narbo/Translator/MainActivity.java
@@ -1,4 +1,4 @@
-package narbo.ir.narbo;
+package narbo.ir.narbo.Translator;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import narbo.ir.narbo.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/narbo/ir/narbo/Translator/View/IntroScreen.java
@@ -0,0 +1,20 @@
+package narbo.ir.narbo.Translator.View;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import narbo.ir.narbo.R;
+
+/**
+ * Created by JL on 1/29/2017.
+ */
+
+public class IntroScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+
+}
